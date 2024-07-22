697ecbf65924398e841605cdf1073107b19eb1af
kimsy98 <119915194+kimsy98@users.noreply.github.com>
2024-07-22 17:43:06 +0900

    [Gold IV] Title: 수 묶기, Time: 132 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Gold/1744. 수 묶기/수 묶기.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        List<Integer> pn = new ArrayList<>();
+        List<Integer> nn = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num > 0) {
+                pn.add(num);
+            } else nn.add(num);
+        }
+
+        // 큰 수부터 곱하기
+        pn.sort(Collections.reverseOrder());
+        // 절대값이 큰 수부터 곱하기
+        Collections.sort(nn);
+
+        int answer = 0;
+        for (int i = 0; i < pn.size(); i++) {
+        	// 1일 경우 유의!!
+            if (i + 1 < pn.size() && pn.get(i) != 1 && pn.get(i + 1) != 1) {
+                answer += pn.get(i) * pn.get(i + 1);
+                i++;
+            } else answer += pn.get(i);
+        }
+        
+        for (int i = 0; i < nn.size(); i++) {
+            if (i + 1 < nn.size()) {
+                answer += nn.get(i) * nn.get(i + 1);
+                i++;
+            } else answer += nn.get(i);
+        }
+
+        System.out.println(answer);
+        br.close();
+	}
+
+}
