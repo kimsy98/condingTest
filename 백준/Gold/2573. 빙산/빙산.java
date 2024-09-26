68b615fcf560b591c37cacfc350776e7a78676eb
kimsy98 <119915194+kimsy98@users.noreply.github.com>
2024-09-26 13:05:04 +0900

    [Gold IV] Title: 빙산, Time: 568 ms, Memory: 299512 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -1,156 +1,132 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-
-
-/**
- * 
- * 녹일 때는 BFS 로 돌고 덩어리 셀때는 dfs로 돌아보자
- * bfs1년 녹이고 그다음에 dfs로 넘어가서 세고 두 덩어리 이상 안될때는 어카냐?
- * @author SSAFY
- *
- */
 public class Main {
-	
-	static class Node{
-		int r,c;
 
-		public Node(int r, int c) {
-			super();
-			this.r = r;
-			this.c = c;
+	static int n;
+	static int m;
+	static int[][] iceberg;
+	static boolean[][] visit;
+	static int[] dx = {0,1,-1,0};
+	static int[] dy = {1,0,0,-1};
+	static int res=1;
+	
+	static private class Node{
+		int x,y;
+		Node(int x, int y){
+			this.x = x;
+			this.y = y;
 		}
-		
-		
 	}
 	
-	static int n,m;
-	static int year;
-	static int landCnt;
-	static int[][] map;
-	static int[][] copymap;
-	static boolean[][] visit;
-	static Queue<Node> q = new ArrayDeque<>();
-	static int[] dx = {1,0,-1,0};
-	static int[] dy = {0,1,0,-1};
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
+		
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 		
+		iceberg = new int[n][m];
 		visit = new boolean[n][m];
-		map = new int[n][m];
-		copymap = new int[n][m];
 		
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < m; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());	
+				iceberg[i][j] = Integer.parseInt(st.nextToken());		
 			}
 		}
 		
-		bfs();
-		if(landCnt<=1) {
-			System.out.println(0);
-		}else {
-			
-			System.out.println(year);
-		}
-		
-		
-		
-	}
-	private static void bfs() {
-		
-		
-		
-		while(true) {
-			
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < m; j++) {
-					if(map[i][j]>0)q.add(new Node (i,j));
-				}
-			}
-			if(q.size()==0)break;
-			copymap = new int[n][m];
+		melt();
+		System.out.println(res);
 
-			while (!q.isEmpty()) {
-				Node node = q.poll();
-				
-				int row = node.r;
-				int col = node.c;
-				int cnt = 0;
-				for (int i = 0; i < 4; i++) {
-					int nr = row + dx[i];
-					int nc = col + dy[i];
-					if(nr<0||nr>=n||nc<0||nc>=m||map[nr][nc]>0)continue;
-					cnt++;
-				}
-				copymap[row][col]= cnt;
-				
-			}
-			
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < m; j++) {
-					if(copymap[i][j]!=0) {
-						map[i][j] = map[i][j] - copymap[i][j];
-					}	
-				}
-			}
-			
-//			System.out.println("map change :");
-//			for (int i = 0; i < n; i++) {
-//				
-//				System.out.println(Arrays.toString(map[i]));
-//			}
-			
-			
-			
-			
-			year++;
-			landCnt = 0;
-			visit = new boolean[n][m];
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < m; j++) {
-					if(map[i][j]>0 && !visit[i][j]) {
-						
-//						System.out.println("landCnt : " + landCnt);
-						landCntdfs(i,j);
-						landCnt++;
-					}
-				}
-			}
-			
-			if(landCnt>=2)break;
-		}
-		
 	}
-	private static void landCntdfs(int row, int col) {
-		visit[row][col] = true;
-//		System.out.print(row+" " +col+"::");
-//		for (int i = 0; i < n; i++) {
-//			
-//			System.out.println(Arrays.toString(visit[i]));
-//		}
+	static void melt() {
+		int[][] copyarr = new int[n][m];
+		visit = new boolean[n][m];
 		
-		for (int k = 0; k < 4; k++) {
-			int nr = row + dx[k];
-			int nc = col + dy[k];
-			if(nr<0||nr>=n||nc<0||nc>=m)continue;
-			if(map[nr][nc]>0&&!visit[nr][nc]) {
-				landCntdfs(nr,nc);
-			}
-	
+		for (int i = 0; i < n; i++) {
+			copyarr[i] = iceberg[i].clone();
 		}
 		
+		//1년 지남
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				
+				int cnt = 0;
+				//빙산이면 사방탐색 후 카운트
+				if(copyarr[i][j]!=0) {
+					for (int dir = 0; dir < 4; dir++) {
+						int x = i+dx[dir];
+						int y = j+dy[dir];
+						if(iceberg[x][y]==0)cnt++;
+						
+					}
+					//복사arr에 저장
+					if(iceberg[i][j]-cnt<=0) copyarr[i][j] = 0;
+					else copyarr[i][j] = iceberg[i][j] - cnt;
+				}
+			}
+		}
+		
+//		for (int i = 0; i < n; i++) {
+//			System.out.println(Arrays.toString(copyarr[i]));
+//		}
+
+        
+		//iceberg 배열에 다시 깊은복사
+		for (int i = 0; i < n; i++) {
+			iceberg[i] = copyarr[i].clone();
+		}
+
+		//빙산 분리되었는지 체크(bfs)		
+		int land = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if(iceberg[i][j]!=0&& !visit[i][j]) {
+					bfs(i,j);
+					land++;
+				}
+			}
+		}
+	
+		//빙하가 분리되거나 다 녹을때까지 재귀
+		if(land==0) {
+			res = 0;
+			return;
+		}else if(land>1){
+			return;
+		}else {
+			res++;
+			melt();
+		}
+	
+	}
+    
+	static void bfs(int x, int y) {
+		Queue<Node> q = new LinkedList<>();
+		q.add(new Node(x,y));
+		
+		while(!q.isEmpty()) {
+			Node node = q.poll();
+			int row = node.x;
+			int col = node.y;
+			
+			for (int i = 0; i < 4; i++) {
+				int nx = row+dx[i];
+				int ny = col+dy[i];
+				if(iceberg[nx][ny]!=0 && !visit[nx][ny]) {
+					visit[nx][ny] = true;
+					q.add(new Node(nx, ny));
+				}
+			}
+			
+		}
 		
 	}
 
