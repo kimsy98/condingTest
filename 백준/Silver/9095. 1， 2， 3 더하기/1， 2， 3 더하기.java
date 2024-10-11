f15c78c859fb28fdd11393c4eb73148cea93d6a1
kimsy98 <119915194+kimsy98@users.noreply.github.com>
2024-10-11 11:44:20 +0900

    Update 1， 2， 3 더하기.java

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -4,23 +4,25 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Scanner sc  = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
 		int[] dp = new int[11];
-		dp[3] = 4;
+		
 		dp[1] = 1;
-		dp[2] = 2;		
-
-		for (int j = 4; j <=10; j++) {
-			dp[j] = dp[j-1]+dp[j-2]+dp[j-3];
+		dp[2] = 2;
+		dp[3] = 4;
+		
+		for (int i = 4; i < 11; i++) {
+			dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
 		}
 		
-		int t = sc.nextInt();
-		
-		for (int i = 0; i < t; i++) {			
-			int n = sc.nextInt();
-			System.out.println(dp[n]);
+		int tc = sc.nextInt();
+		for (int i = 0; i < tc; i++) {
+			int num = sc.nextInt();
+			System.out.println(dp[num]);
 		}
+		
+		
 	}
 
 }
