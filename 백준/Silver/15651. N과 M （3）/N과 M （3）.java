60538f351a22273b2ca5459962bb9d51e93dedb9
kimsy98 <119915194+kimsy98@users.noreply.github.com>
2024-10-12 19:17:33 +0900

    [Silver III] Title: N과 M (3), Time: 440 ms, Memory: 71204 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,38 +1,33 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
 	static int n,m;
-	static int[] arr;
-	static boolean[] check;
+	static int res[];
 	static StringBuilder sb = new StringBuilder();
+	
 	public static void main(String[] args) {
-
+		
 		Scanner sc = new Scanner(System.in);
 		
 		n = sc.nextInt();
 		m = sc.nextInt();
+		res = new int[m];
+		permu_repe(0);
 		
-		arr = new int[m];
-		check = new boolean[n];
-		
-		dfs(0);
 		System.out.println(sb);
 	}
-	static void dfs(int cnt) {
-		if(cnt==m) {
-			for(int num : arr) {
-				sb.append(num+" ");
+	static void permu_repe(int dep) {
+		if(dep==m) {
+			for(int num:res) {
+				sb.append(num).append(' ');
 			}
-			sb.append("\n");
+			sb.append('\n');
 			return;
 		}
-		for (int i = 0; i < n; i++) {
-			check[i] = true;
-			arr[cnt] = i+1;
-			dfs(cnt+1);
-			check[i] = false;
+		for (int i = 1; i <=n; i++) {
+			res[dep] = i;
+			permu_repe(dep+1);
 		}
 	}
 
