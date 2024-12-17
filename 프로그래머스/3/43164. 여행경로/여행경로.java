49c6287a570716b187ad6e9990e684dceaf87581
kimsy98 <119915194+kimsy98@users.noreply.github.com>
2024-12-17 13:45:42 +0900

    [level 3] Title: 여행경로, Time: 77.74 ms, Memory: 119 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    static boolean[] vis;
+    static List<String> li = new ArrayList<>();
+    
+    public String[] solution(String[][] tickets) {
+        String[] answer = {};
+        vis = new boolean[tickets.length];
+        
+        dfs(0,"ICN", "ICN", tickets);
+        Collections.sort(li);
+        answer = li.get(0).split(" ");
+        return answer;
+    }
+    static void dfs(int dep, String plan, String now, String[][] tickets){
+        if(dep==tickets.length){
+            li.add(plan);
+        }
+        for(int i=0; i<vis.length;i++){
+            if(!vis[i]&&now.equals(tickets[i][0])){
+                vis[i] =true;
+                dfs(dep+1,plan+" "+tickets[i][1], tickets[i][1], tickets);
+                vis[i] = false;
+            }
+        }
+    }
+}
