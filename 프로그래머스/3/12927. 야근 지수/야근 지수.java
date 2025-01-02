ca7374f68b02c4afd33dd0cacec99fd4702f1485
kimsy98 <119915194+kimsy98@users.noreply.github.com>
2025-01-02 14:58:46 +0900

    [level 3] Title: 야근 지수, Time: 131.37 ms, Memory: 70.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public long solution(int n, int[] works) {
+        long answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int work : works){
+            pq.add(work);
+        }
+        while(n>0){
+            if(!pq.isEmpty()){
+                int work = pq.poll();
+                n--;
+                work--;
+                if(work>0)pq.add(work);
+            }else{
+                break;
+            }
+        }
+        while(!pq.isEmpty()){
+            int val = pq.poll();
+            answer +=(val*val);
+        }
+        return answer;
+    }
+}
