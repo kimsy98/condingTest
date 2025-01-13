f2c841eda623a5a5019a0bc4cf07734c8a4aa8b0
kimsy98 <119915194+kimsy98@users.noreply.github.com>
2025-01-13 17:55:31 +0900

    [level 3] Title: 베스트앨범, Time: 4.94 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,41 +1,35 @@
 import java.util.*;
- 
+
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        List<Integer> answer = new ArrayList<>();
-        
-        HashMap<String, HashMap<Integer, Integer>> map = new HashMap<>();
-        HashMap<String, Integer> topList = new HashMap<>();
-        for(String genre : genres){
-            map.put(genre, new HashMap<>());
-        }
-        
-        for(int i =0; i<plays.length; i++){
-            String genre = genres[i];
-            int play = plays[i];
-            
-            map.get(genre).put(i, play);
-            // System.out.println(map.get(genre));
-        }
-        for(String gen : map.keySet()){
-            int totalPlay = 0;
-            for(int i : map.get(gen).keySet()){
-                totalPlay+= map.get(gen).get(i);
-            }
-            topList.put(gen, totalPlay);
-        }
-        List<String> keySet = new ArrayList<>(topList.keySet());
-        keySet.sort(((o1,o2)->topList.get(o2)-topList.get(o1)));
 
-        for(String gen :keySet){
-            List<Integer> playKeySet = new ArrayList<>(map.get(gen).keySet());
-            playKeySet.sort(((o1,o2)->map.get(gen).get(o2)-map.get(gen).get(o1)));
-            answer.add(playKeySet.get(0));
-            if(playKeySet.size()>1) answer.add(playKeySet.get(1));
-        } 
+        List<Integer> li = new ArrayList<>();
+        HashMap<String, Integer> genCnt = new HashMap<>();
+        HashMap<String,HashMap<Integer, Integer>> topList = new HashMap<>();
+        for(int i =0; i<plays.length;i++){
+            genCnt.put(genres[i], genCnt.getOrDefault(genres[i],0)+plays[i]);
+            topList.put(genres[i],new HashMap<>());
+        }
+        System.out.println(topList.get(genres[0]));
+
+        for(int i =0; i<plays.length;i++){
+            HashMap<Integer, Integer> play = new HashMap<>();
+            topList.get(genres[i]).put(i,plays[i]);
+        }
         
-        return  answer.stream().mapToInt(i->i).toArray();
+        List<String> keySet = new ArrayList<>(genCnt.keySet());
+        keySet.sort((o1,o2)-> genCnt.get(o2).compareTo(genCnt.get(o1)));
+        
+        for(String key : keySet){
+            List<Integer> topListKeySet = new ArrayList<>(topList.get(key).keySet());
+            topListKeySet.sort((o1,o2)-> topList.get(key).get(o2).compareTo(topList.get(key).get(o1)));
+            li.add(topListKeySet.get(0));
+            if(topListKeySet.size()>1)li.add(topListKeySet.get(1));
+
+        }
+        System.out.println(li.toString());
+        
+
+        return li.stream().mapToInt(Integer::intValue).toArray();
     }
-        
-        
 }
