587b33215538a7c4bdbb986ec805a9542d259cd5
kimsy98 <119915194+kimsy98@users.noreply.github.com>
2025-02-04 12:22:39 +0900

    [level 2] Title: 튜플, Time: 100.30 ms, Memory: 96 MB -BaekjoonHub

---
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String s) {
+        s = s.substring(2,s.length()-2).replace("},{", "/");
+        
+        String[] sArr = s.split("/");
+        
+        Arrays.sort(sArr, Comparator.comparingInt(String :: length));
+        List<Integer> li = new ArrayList<>();
+        for(String str : sArr){
+            String[] find = str.split(",");
+            for(int i=0; i<find.length; i++){
+                int num = Integer.parseInt(find[i]);
+                if(!li.contains(num)){
+                    li.add(num);
+                }
+            }
+        }
+        return li.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
