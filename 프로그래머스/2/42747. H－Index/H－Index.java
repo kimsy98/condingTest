972bde5be1f080804ba99b392fc50605476493f0
kimsy98 <119915194+kimsy98@users.noreply.github.com>
2025-02-13 14:19:24 +0900

    [level 2] Title: H-Index, Time: 0.97 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -3,10 +3,9 @@ class Solution {
     public int solution(int[] citations) {
         int answer = 0;
         Arrays.sort(citations);
-        for(int i =0; i<citations.length;i++){
-            if(citations[i]>=citations.length-i){
+        for(int i = citations.length-1; i>=0 ; i--){
+            if(citations.length-i<=citations[i]){
                 answer = citations.length-i;
-                break;
             }
         }
         return answer;
