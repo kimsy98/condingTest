1ccf062787250fe78028792e692cf7249484889c
kimsy98 <119915194+kimsy98@users.noreply.github.com>
2025-02-13 13:05:42 +0900

    [level 1] Title: K번째수, Time: 0.58 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,27 +1,21 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        
         int[] answer = new int[commands.length];
         
-        for(int i = 0 ; i<answer.length; i++){
-            int st  = commands[i][0];
-            int end = commands[i][1];
-            int step = commands[i][2];
+        for(int tc =0; tc<commands.length;tc++){
+            int i = commands[tc][0]-1;
+            int j = commands[tc][1]-1;
+            int k = commands[tc][2]-1;
+            int size = j-i+1;
             
-            int[] arr = new int[end-st+1];
-            int cnt = 0;
-            for(int j =st-1; j<=end-1; j++){
-                arr[cnt++] = array[j];
+            int[] arr = new int[size];
+            for(int st =0;st<size;st++){
+                arr[st] = array[st+i];
             }
-            
             Arrays.sort(arr);
-            answer[i] = arr[step-1];
-            
+            answer[tc] = arr[k];
         }
-        
-        
         return answer;
     }
 }
