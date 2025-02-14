751372597b72b21b0e53fc311189efbbbbc45db3
kimsy98 <119915194+kimsy98@users.noreply.github.com>
2025-02-14 16:16:22 +0900

    [level 1] Title: 폰켓몬, Time: 6.63 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,11 +1,13 @@
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
+import java.util.*;
 class Solution {
     public int solution(int[] nums) {
-        return Arrays.stream(nums)
-                .boxed()
-                .collect(Collectors.collectingAndThen(Collectors.toSet(),
-                        phonekemons -> Integer.min(phonekemons.size(), nums.length / 2)));
+        int answer = 0;
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums){
+            set.add(num);
+        }
+        answer = set.size();
+        if(nums.length/2<= set.size())answer = nums.length/2;
+        return answer;
     }
 }
