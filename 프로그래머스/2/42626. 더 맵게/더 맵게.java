27b7dc583d0da14475d2e2c5924a7ae927778683
kimsy98 <119915194+kimsy98@users.noreply.github.com>
2025-02-15 15:08:54 +0900

    [level 2] Title: 더 맵게, Time: 1941.57 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,19 +1,22 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int s : scoville)pq.add(s);
-        
-        int min = pq.peek();
-        
+        for(int scov : scoville){
+            pq.add(scov);
+        }
+        int min = pq.peek().intValue();
+        // System.out.println(min);
         while(min<K){
             if(pq.size()>=2){
-                pq.add(pq.poll()+(pq.poll()*2));
-                min = pq.peek();
                 answer++;
-            }else return -1;
+                int num = pq.poll()+(pq.poll()*2);
+                pq.add(num);
+                min = pq.peek();
+            }else{
+                return -1;               
+            }
         }
         
         return answer;
