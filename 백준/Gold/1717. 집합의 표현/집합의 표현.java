6d8673039c38b8155b39e94e678edae9770e434f
kimsy98 <119915194+kimsy98@users.noreply.github.com>
2025-02-21 18:26:17 +0900

    [Gold V] Title: 집합의 표현, Time: 400 ms, Memory: 49968 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -2,69 +2,65 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.stream.IntStream;
 
 public class Main {
 
 	static int n,m;
 	static int[] parent;
+	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 
 		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
+		
 		
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
+		parent = IntStream.range(0, n+1).toArray();
 		
-		parent = new int[n+1];
-		
-		for(int i=0; i<n+1;i++) {
-			parent[i] = i;
-		}
-		
-		for(int i =0;i<m;i++) {
-			
+		for(int i=0; i<m; i++) {
 			st = new StringTokenizer(br.readLine());
+			int oper = Integer.parseInt(st.nextToken());
+			int a= Integer.parseInt(st.nextToken());
+			int b= Integer.parseInt(st.nextToken());
 			
-			int cal = Integer.parseInt(st.nextToken());
-			
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			
-			if(cal==0){
+			if(oper==0) {
 				union(a,b);
-			}
-			else if (cal==1){
+			}else {
 				int x = find(a);
-				int y =find(b);
-				
+				int y = find(b);
+//				check(x,y);
 				if(x==y) sb.append("YES").append("\n");
 				else sb.append("NO").append("\n");
+				
 			}
 		}
-		
+
 		System.out.println(sb);
 		
-		
 	}
-	
-    static void union(int a, int b) {
-        int x = find(a);
-        int y = find(b);
-
-        if (x < y) {
-            parent[y] = x;
-        } else {
-            parent[x] = y;
-        }
-    }
+	static void union(int a, int b) {
+		int x = find(a);
+		int y = find(b);
+		
+		if(x < y) {
+			parent[y] = x;
+		}else parent[x] = y;
+	}
 	static int find(int a) {
 		
-		if(parent[a]==a) return a;
+		if(parent[a]==a)return a;
 		return parent[a] = find(parent[a]);
-		
 	}
-
+	static void check(int x, int y) {
+		if(x==y) {
+			sb.append("YES").append("\n");
+		}else {
+			sb.append("No").append("\n");
+		}
+	}
+	
 }
