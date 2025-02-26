4514b79a73459d70e6b58c6edc105e082b97d1f2
kimsy98 <119915194+kimsy98@users.noreply.github.com>
2025-02-26 15:43:10 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 5.93 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,51 +1,60 @@
 import java.util.*;
-class Solution {
-    static boolean[] vis;
-    static List<Integer>[] wireList;
-    static int answer = Integer.MAX_VALUE;
-    public int solution(int n, int[][] wires) {
 
-        wireList = new ArrayList[n+1];
-        
-        for(int i =0; i<=n; i++){
-            wireList[i] = new ArrayList<>();
-        }
-        for(int[] wire : wires){
-            int a = wire[0];
-            int b = wire[1];
-            wireList[a].add(b);
-            wireList[b].add(a);  
-        }
-        for(int i =1 ; i<=n; i++){
-            for(int j =0; j<wireList[i].size(); j++){
-                int copy = wireList[i].get(j);
-//                 wireList[i].remove(j);
-//                 wireList[copy].remove(Integer.valueOf(i));
-                
-                vis = new boolean[n+1];
-                vis[i] = true;
-                vis[copy] = true;
-                int Grid1 = dfs(n, i, 1);
-                
-                // vis = new boolean[n+1];
-                int Grid2 = dfs(n, copy, 1);
-                
-                // System.out.println("dfs Cnt : "+Grid1 +" "+ Grid2);
-                // System.out.println("list "+ wireList[i]+ " // "+wireList[copy]);
-                answer = Math.min(answer, Math.abs(Grid1-Grid2));
-                // wireList[i].add(copy);
-                // wireList[copy].add(i);
-            }
+import static java.lang.Math.*;
+class Solution {
+    public int solution(int n, int[][] wires) {
+        int answer = Integer.MAX_VALUE;
+        int m = wires.length;
+        for(int i = 0; i < m; i++){
+            int[] parent = makeParentArr(n);
+            answer = min(answer, solve(parent, i, wires));
         }
         return answer;
     }
-    static int dfs(int n ,int idx, int cnt){
 
-        for(int i=0; i<wireList[idx].size(); i++){
-            if(vis[wireList[idx].get(i)]) continue;
-            vis[wireList[idx].get(i)] = true;
-            cnt = dfs(n, wireList[idx].get(i), cnt+1);
+    private int solve(int[] parent, int except, int[][] wires){
+        int n = wires.length;
+        for(int i = 0; i < n; i++){
+            if(i == except) continue;
+            int[] v = wires[i];
+            int a = v[0], b = v[1];
+            union(a, b, parent);
         }
-        return cnt;
+        for(int i = 1; i < parent.length; i++){
+            find(i, parent);
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int v : parent){
+            if(v == 0) continue;
+            map.put(v, map.getOrDefault(v, 0) + 1);
+        }
+        int[] tmp = new int[2];
+        int idx = 0;
+        for(int key : map.keySet()){
+            tmp[idx++] = map.get(key);
+        }
+        return abs(tmp[0] - tmp[1]);
+    }
+
+    private int find(int v, int[] parent){
+        if(parent[v] != v){
+            parent[v] = find(parent[v], parent);
+        }
+        return parent[v];
+    }
+
+    private void union(int a, int b, int[] parent){
+        int aParent = find(a, parent);
+        int bParent = find(b, parent);
+
+        parent[bParent] = aParent;
+    }
+
+    private int[] makeParentArr(int n){
+        int[] ret = new int[n+1];
+        for(int i = 0; i <= n; i++){
+            ret[i] = i;
+        }
+        return ret;
     }
 }
