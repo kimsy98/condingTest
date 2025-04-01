f66e625e3e8178bf67c1b5432fc768d592defc65
kimsy98 <119915194+kimsy98@users.noreply.github.com>
2025-04-01 13:08:29 +0900

    [Gold IV] Title: 연구소, Time: 388 ms, Memory: 161048 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -7,92 +7,98 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+	
 	static int n,m;
-	static int[][] arr;
-	static boolean[][] check;
-	static int cnt=0;
-	static class Node{
-		int x,y;
-		public Node(int x, int y) {
-			this.x = x;
-			this.y= y;
-		}
-	}
-	static int[] dx = {-1,0,1,0};
-	static int[] dy = {0,1,0,-1};
+	static int[][] map;
+	static boolean[][] visit;
+	static int[] nx = {0,1,-1,0};
+	static int[] ny = {1,0,0,-1};
+	static int answer=0;
+//	static int cnt = 0;
 	public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		n= Integer.parseInt(st.nextToken());
-		m= Integer.parseInt(st.nextToken());
 		
-		arr = new int[n][m];
-		check = new boolean[n][m];
-		for (int i = 0; i < n; i++) {
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+	
+		map = new int[n][m];
+		visit  = new boolean[n][m];
+		
+		for(int i =0; i<n; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < m; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
+			for(int j = 0; j<m; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+//				if(map[i][j]==1)cnt++;
+				
 			}
 		}
-		dfs(0,0);
-		
-		System.out.println(cnt);
+		combi(0,0);
+		System.out.println(answer);
 		
 	}
-	static void dfs(int start, int wall) {
+	static void combi(int st, int wall) {
 		if(wall==3) {
 			count();
 			return;
 		}
-		for (int i = start; i <n*m ; i++) {
-			if(!check[i/m][i%m]&&arr[i/m][i%m]==0) {
-				check[i/m][i%m] = true;
-				arr[i/m][i%m] = 1;
-				dfs(i,wall +1);
-				arr[i/m][i%m] = 0;
-				check[i/m][i%m] = false;
+		for(int i = st; i<n*m;i++) {
+			if(!visit[i/m][i%m]&&map[i/m][i%m]==0) {
+				 visit[i/m][i%m] = true;
+				 map[i/m][i%m] = 1;
+				 combi(i,wall+1);
+				 visit[i/m][i%m] = false;
+				 map[i/m][i%m] = 0;
+				 
 			}
 		}
 	}
-	static void count() {
-		Queue<Node> q = new LinkedList<>();
-		boolean[][] visit = new boolean[n][m];
-		int[][] arrCopy = new int[n][m];
-		
-		for (int i = 0; i < n; i++) {
-			arrCopy[i] = arr[i].clone();
+	static class position{
+		int x;
+		int y;
+		position(int x, int y){
+			this.x = x;
+			this.y=  y;
 		}
-		
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				if(arr[i][j]==2)q.add(new Node(i,j));
+	}
+	static void count() {
+		int cnt =0;
+		Queue<position> q = new LinkedList<>();
+		int[][] copyMap = new int[n][m];
+		boolean[][] copyVisit = new boolean[n][m];
+		for(int i =0; i<n; i++) {
+			copyMap[i] = map[i].clone();
+//			copyVisit[i] = visit[i].clone();
+		}
+		for(int i =0; i<n; i++) {
+			for(int j =0; j<m; j++) {
+				if(copyMap[i][j]==2)q.add(new position(i, j));
 			}
 		}
 		while(!q.isEmpty()) {
-			Node node = q.poll();
+			position pos = q.poll();
+			int x = pos.x;
+			int y = pos.y;
 			
-			for (int i = 0; i < 4; i++) {
-				int nx = node.x+dx[i];
-				int ny = node.y+dy[i];
+			for(int i =0; i<4; i++) {
+				int dx = x+nx[i];
+				int dy = y+ny[i];
 				
-				if(nx<0||nx>=n||ny<0||ny>=m)continue;
-				if(visit[nx][ny]||arr[nx][ny]==1)continue;
+				if(dx<0||dx>=n||dy<0||dy>=m)continue;
+				if(copyVisit[dx][dy]||copyMap[dx][dy]==1)continue;
 				
-				visit[nx][ny] = true;
-				q.add(new Node(nx,ny));
-				arrCopy[nx][ny] = 2;
-			}
-			
-		}
-		int num = 0;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				if(arrCopy[i][j]==0)num++;
+				copyMap[dx][dy] = 2;
+				copyVisit[dx][dy] = true;
+				q.add(new position(dx, dy));
 			}
 		}
-		
-		cnt = Math.max(num, cnt);
+		for(int i =0; i<n; i++) {
+			for(int j =0; j<m; j++) {
+				if(copyMap[i][j]==0) cnt++;
+			}
+		}
+		answer = Math.max(answer, cnt);
 		
 	}
 
