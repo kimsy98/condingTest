94f867de86521641be50ed46fe508107a639c538
kimsy98 <119915194+kimsy98@users.noreply.github.com>
2025-05-01 22:34:38 +0900

    [level 2] Title: [1차] 캐시, Time: 33.40 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -3,37 +3,29 @@ import java.util.*;
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
-        if(cacheSize==0)return answer+=cities.length*5;
+        LinkedHashSet<String> cache = new LinkedHashSet<>();
 
-        Deque<String> dq = new LinkedList<>();
-        Stack<String> st = new Stack<>();
-        for(String city : cities){
+        for (String city : cities) {
             city = city.toLowerCase();
-            if(!dq.contains(city)){
-                if(dq.size()>=cacheSize){
-                    dq.poll();    
-                }
-                dq.add(city);
-                answer+=5;
-                            // System.out.println(dq);
-            }else{
-                while(true){
-                    String now = dq.poll();
-                    if(now.equals(city)){
-                        answer++;
-                        dq.add(city);
-                        while(!st.isEmpty()){
-                            dq.offerFirst(st.pop());
-                        }
-                        break;
-                    }
-                    st.push(now);
-                }
-                            // System.out.println(dq);
+            if (cacheSize == 0) {
+                answer += 5;
+                continue;
             }
 
+            if (cache.contains(city)) {
+                cache.remove(city);
+                cache.add(city);
+                answer += 1;
+            } else {
+                if (cache.size() >= cacheSize) {
+                    Iterator<String> it = cache.iterator();
+                    it.next();
+                    it.remove();
+                }
+                cache.add(city);
+                answer += 5;
+            }
         }
-        
         return answer;
     }
 }
