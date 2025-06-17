907cad7ab43ccefe6fbc6063babf611c843a00b6
kimsy98 <119915194+kimsy98@users.noreply.github.com>
2025-06-17 14:08:28 +0900

    [level 3] Title: 양과 늑대, Time: 3.20 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,37 +1,34 @@
+import java.util.*;
 class Solution {
-    static int answer = 0;
+    int answer = 0;
+    List<Integer>[] edg;
     public int solution(int[] info, int[][] edges) {
-        boolean[] vis = new boolean[info.length];
-        dfs(info, edges, 0, 0,0, vis);
-        
+        edg = new List[info.length];
+        for(int i =0; i<edges.length; i++){
+            edg[i] = new ArrayList<>();
+        }
+        int remainSheep = Collections.frequency(Arrays.asList(info), 0);
+        bfs(1,0,0,remainSheep, info,edges);
         return answer;
     }
-    public void dfs(int[] info, int[][] edges, int idx,int sheepCnt, int wolfCnt, boolean[] vis){
-        vis[idx] = true;
-        if(info[idx]==0){
-            sheepCnt++;
-            answer = Math.max(answer, sheepCnt);
-        }else{
-            if(sheepCnt <=wolfCnt+1){
-                return;
-            }
-            wolfCnt++;
-        }
-        
-        for(int i =0; i<edges.length;i++){
-            int[] edge = edges[i];
-            int start = edge[0];
-            int end = edge[1];
-            if(vis[start]&&!vis[end]){
-                boolean[] visitArr = new boolean[info.length];
-                for(int j =0; j< vis.length; j++){
-                    visitArr[j] = vis[j];
+    public void bfs(int sheep,int wolf,int root,int remainSheep, int[] info, int[][] edges){
+        Queue<int[]> q = LinkedList<>();
+        //sheep,wolf, index
+        q.add(new int[] {1,0, root});
+        info[0] = 0;
+        while(!q.isEmpty()){
+            int[] status = q.poll();
+            int shCnt = status[0];
+            int woCnt = status[1];
+            int idx = status[2];
+            for(int i =0; i<edges[idx].size(); i++){
+                int nextIdx = edges[idx].get(i);
+                if(nextIdx==0){
+                    while(true){
+                        edges[nextIdx]
+                    }
                 }
-                dfs(info, edges, end, sheepCnt, wolfCnt, visitArr);
             }
-            
-            
         }
-        
     }
 }
