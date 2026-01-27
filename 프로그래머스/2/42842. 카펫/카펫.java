25c11b660173b44d297b3b96903222e6efcfc8ce
kimsy98 <119915194+kimsy98@users.noreply.github.com>
2026-01-27 15:02:02 +0900

    [level 2] Title: 카펫, Time: 0.08 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,19 +1,15 @@
-
+import java.util.*;
+import java.io.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        int range = (int)Math.floor(Math.sqrt((double)yellow));
-        System.out.println(range);
-        for(int i=1;i<=range;i++){
-            if(yellow%i==0){
-                int width = yellow/i;
-                int size = (i+2)*2+(width*2);
-                if(size==brown){
-                    answer[0] = width+2;
-                    answer[1] = i+2;
-                    break;
-                }
-            }
+        
+        int sqrt =(int)Math.sqrt(yellow);
+        for(int i =1; i<=sqrt; i++){
+            if(yellow%i!=0)continue;
+            if((i+2)*(yellow/i+2)-yellow!=brown)continue;
+            answer[0] = yellow/i+2;
+            answer[1] = i+2;
         }
         return answer;
     }
