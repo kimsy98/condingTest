825565ea27c599fa8b4ab690a49207fa238c5297
kimsy98 <119915194+kimsy98@users.noreply.github.com>
2026-03-06 14:22:21 +0900

    [level 2] Title: 스킬트리, Time: 0.08 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -2,38 +2,24 @@ import java.util.*;
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
-        Queue<Character> q = new LinkedList<>();
-        for(int i = 0; i<skill.length(); i++){
-            q.add(skill.charAt(i));
-        }
         
-        List<Queue<Character>> skillList= new ArrayList<>();
-        
-        for(int i = 0; i< skill_trees.length; i++){
-            skillList.add(new LinkedList<>());
-            for(int j = 0; j<skill_trees[i].length(); j++){
-                if(q.contains(skill_trees[i].charAt(j))){
-                    skillList.get(i).add(skill_trees[i].charAt(j));
+        for(String skill_tree : skill_trees){
+            boolean check = true;
+            int nowIdx = 0;
+            
+            for(char s : skill_tree.toCharArray()){
+                int skillIdx = skill.indexOf(s);
+                if(skillIdx==-1)continue;
+                if(skillIdx==nowIdx){
+                    nowIdx++;
+                }else{
+                    check = false;
+                    break;
                 }
+                
             }
-            
+            if(check) answer++;
         }
-        
-        for(Queue<Character> li : skillList){
-            Queue<Character> tempQ = new LinkedList<>();
-            for(int i = 0; i<skill.length(); i++){
-                tempQ.add(skill.charAt(i));
-            }
-            while(!tempQ.isEmpty()&&!li.isEmpty()){
-                if(tempQ.peek()==li.peek()){
-                    tempQ.poll();
-                    li.poll();
-                }else break;
-            }
-            if(li.isEmpty())answer++;
-            // System.out.println(li);
-            
-        }        
         
         return answer;
     }
