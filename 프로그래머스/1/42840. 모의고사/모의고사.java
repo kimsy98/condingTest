016137586e44e42df0c7ca7f905892e295569d03
kimsy98 <119915194+kimsy98@users.noreply.github.com>
2026-03-09 14:48:42 +0900

    [level 1] Title: 모의고사, Time: 3.57 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,26 +1,25 @@
 import java.util.*;
 class Solution {
+    int[] supo1 = {1,2,3,4,5};
+    int[] supo2 = {2,1,2,3,2,4,2,5};
+    int[] supo3 = {3,3,1,1,2,2,4,4,5,5};
     public int[] solution(int[] answers) {
-        int[] ott = new int[3];
-        int[] oneArray = {1,2,3,4,5};
-        int[] twoArray = {2,1,2,3,2,4,2,5};
-        int[] threeArray = {3,3,1,1,2,2,4,4,5,5};
+        int supo1Score = 0;
+        int supo2Score = 0;
+        int supo3Score = 0;
+        for(int i = 0 ; i<answers.length; i++){
+            if(supo1[i%supo1.length]==answers[i])supo1Score ++;
+            if(supo2[i%supo2.length]==answers[i])supo2Score ++;
+            if(supo3[i%supo3.length]==answers[i])supo3Score ++;
+        }
+        int max = Math.max(supo1Score, Math.max(supo2Score, supo3Score));
 
-        for(int i = 0; i<answers.length;i++){
-            //1번 수포자
-            if(oneArray[i%5]==answers[i])ott[0]++;
-            //2번 수포자          
-            if(twoArray[i%8]==answers[i])ott[1]++;
-            //3번 수포자
-            if(threeArray[i%10]==answers[i])ott[2]++;
-            
-        }
-        int max = Math.max(ott[0],Math.max(ott[1], ott[2]));
         List<Integer> answer = new ArrayList<>();
-        for(int i =0; i< ott.length; i++){
-            if(max==ott[i]) answer.add(i+1);
-        }
+        if(max == supo1Score)answer.add(1);
+        if(max == supo2Score)answer.add(2);
+        if(max == supo3Score)answer.add(3);
+    
         
-        return answer.stream().mapToInt(i->i.intValue()).toArray();
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
