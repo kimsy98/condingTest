759ea493163c1f4c618225ac8b081c5de24a6806
kimsy98 <119915194+kimsy98@users.noreply.github.com>
2026-03-26 19:28:43 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.53 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/README.md
+++ 프로그래머스/3/42627. 디스크 컨트롤러/README.md
@@ -1,10 +1,10 @@
 # [level 3] 디스크 컨트롤러 - 42627 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627?language=java#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627) 
 
 ### 성능 요약
 
-메모리: 79.5 MB, 시간: 3.66 ms
+메모리: 83.9 MB, 시간: 3.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 06일 15:15:45
+2026년 03월 26일 19:28:40
 
 ### 문제 설명
 
--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,42 +1,44 @@
-/***
-정렬 기준:
-    1. 작업 소요시간 짧은 순
-    2. 작업 요청 시각 빠른 순
-    3. 작업 번호 작은 순
-***/
 import java.util.*;
-
 class Solution {
+    /***
+    1. jobs[i][1] (소요시간) 짧은 순, jobs[i][0](요청 시각) 빠른 순, i (작업 번호) 작은 순
+    
+    ***/
     public int solution(int[][] jobs) {
         int answer = 0;
         int n = jobs.length;
-        Arrays.sort(jobs,(o1,o2)->o1[0]-o2[0]);
+        Arrays.sort(jobs, (a,b)->a[0]-b[0]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->{
+            if(a[1]==b[1]){
+                if(a[0]==b[0]){
+                    return a[2]-b[2];
+                }else{
+                    return a[0]-b[0];
+                }
+            }else{
+                return a[1]-b[1];
+            }
+        });
         
-        //0 : 작업 요청 시간, 1 : 작업 소요시간
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1,o2)->o1[1]-o2[1]);
-        
-        int nowTime = jobs[0][0];
-        int idx =0;
+        int nowTime = 0;
+        int idx = 0;
         while(idx<n||!pq.isEmpty()){
-            while(idx<n&&nowTime>=jobs[idx][0]){
-                pq.add(jobs[idx++]);
+            while(idx<n&&jobs[idx][0]<=nowTime){
+                pq.add(new int []{jobs[idx][0], jobs[idx][1], idx});
+                idx++;
             }
             if(pq.isEmpty()){
-                nowTime=jobs[idx][0];
-                pq.add(jobs[idx++]);
+                nowTime = jobs[idx][0];
+                // pq.add(new int[] {jobs[idx][0], jobs[idx][1], idx});
+                // idx++;
+            }
+            else{
+                int[] now = pq.poll();
+                nowTime += now[1];
+                answer += nowTime-now[0];          
             }
 
-            int [] job = pq.poll();
-            int s = job[0];
-            int i = job[1];
-
-            nowTime+=i;
-            answer+=nowTime-s;
-
-
         }
-
-
         return answer/n;
     }
 }
