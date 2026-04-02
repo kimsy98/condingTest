ae1e0d6f49a06cb378ca66bfdc21e689b95a2b17
kimsy98 <119915194+kimsy98@users.noreply.github.com>
2026-04-02 20:50:56 +0900

    [level 3] Title: 순위, Time: 19.02 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,60 +1,35 @@
-import java.util.*;
-
 class Solution {
-    List<List<Integer>> wins = new ArrayList<>();
-    List<List<Integer>> loses = new ArrayList<>();
-    
     public int solution(int n, int[][] results) {
         int answer = 0;
-        
-        for(int i = 0; i <= n; i++) {
-            wins.add(new ArrayList<>());
-            loses.add(new ArrayList<>());
+        int[][] g = new int[n+1][n+1];
+        for(int[] edge : results){
+            g[edge[0]][edge[1]] = 1;
+            g[edge[1]][edge[0]] = -1;
         }
         
-        for(int[] result: results) {
-            int win = result[0];
-            int lose = result[1];
-            wins.get(win).add(lose);
-            loses.get(lose).add(win);
-        }
-        
-        for(int i = 1; i <= n; i++) {
-            boolean[] visited = new boolean[n + 1];
-            Queue<Integer> q = new LinkedList<>();
-            
-            visited[i] = true;
-            q.add(i);
-            while(!q.isEmpty()) {
-                int win = q.poll();
-                for(int lose: wins.get(win)) {
-                    if (visited[lose]) continue;
-                    visited[lose] = true;
-                    q.add(lose);
-                }
-            }
-            
-            q.add(i);
-            while(!q.isEmpty()) {
-                int lose = q.poll();
-                for(int win: loses.get(lose)) {
-                    if (visited[win]) continue;
-                    visited[win] = true;
-                    q.add(win);
-                }
-            }
-            
-            boolean flag = true;
-            for(int j = 1; j <= n; j++) {
-                if(!visited[j]) {
-                    flag = false;
-                }
-            }
-            if (flag) {
-                answer++;
+        for(int i =1; i<=n; i++){
+            for(int j =1; j<=n; j++){
+                for(int k =1; k<=n; k++){
+                    if(g[i][k]==1&&g[k][j]==1){
+                        g[i][j] = 1;
+                        g[j][i] = -1;
+                    }
+                    if(g[i][k]==-1&&g[k][j]==-1){
+                        g[i][j] = -1;
+                        g[j][i] = 1;
+                    }
+                }   
             }
         }
-        
+        for(int i =1; i<=n; i++){
+            int cnt = 0;
+            for(int j =1; j<=n; j++){
+                if(g[i][j]==1||g[i][j]==-1){
+                    cnt++;                    
+                }
+            }
+            if(cnt==n-1)answer++;
+        }
         return answer;
     }
 }
