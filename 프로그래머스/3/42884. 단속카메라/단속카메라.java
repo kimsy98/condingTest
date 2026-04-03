11f9f8d0d355bdf0b8a9c6b7907911d597e48fd3
kimsy98 <119915194+kimsy98@users.noreply.github.com>
2026-04-04 00:14:43 +0900

    [level 3] Title: 단속카메라, Time: 9.97 ms, Memory: 58.6 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,29 +2,23 @@ import java.util.*;
 class Solution {
     public int solution(int[][] routes) {
         int answer = 0;
-        boolean[] visit = new boolean[routes.length];
-        Arrays.sort(routes,(o1,o2)->o1[1]-o2[1]);
-        int start = routes[0][1];
-        int size = routes.length;
-        int idx = 0;
-        int checkCnt = size;
-        while(checkCnt>0){
-            answer++;
-            boolean check=false;
-            for(int i =0; i<size; i++){
-                if(visit[i]) continue;
-                if(routes[i][0]<=start){
-                    visit[i] = true;
-                    checkCnt--;
-                } 
-                else if(!check){
-                    check = true;
-                    idx = i;
-                }
+        
+        Arrays.sort(routes, (a,b)-> a[1]-b[1]);
+        
+        answer++;
+        int carCnt = 1;
+        
+        int lastCctvIdx = routes[0][1];
+        for(int i = 1; i< routes.length; i++){
+            // System.out.println(Arrays.toString(routes[i]));
+            if(routes[i][0]<=lastCctvIdx){
+                carCnt++;        
+            }else{
+                lastCctvIdx = routes[i][1];
+                answer++;
             }
-            start = routes[idx][1];
+            
         }
-
         return answer;
     }
 }
