84a353478e6921c06ab3e427ddc6d6967bccf58f
kimsy98 <119915194+kimsy98@users.noreply.github.com>
2026-04-04 19:29:21 +0900

    [level 3] Title: 네트워크, Time: 0.29 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,30 +1,24 @@
-import java.util.*;
 class Solution {
-    boolean[] vis;
+    int answer = 0;
+    boolean[] visit;
     public int solution(int n, int[][] computers) {
-        int answer = 0;
-        vis = new boolean[n];
-        for(int i = 0; i< computers.length;i++){
-            if(vis[i])continue;
-            answer++;
-            bfs(computers, i);
+        visit = new boolean[n];
+        for(int i = 0 ;i<n; i++){
+            if(visit[i]) continue;
+             visit[i] = true;
+             dfs(computers, i, n, 0);
+             answer++;
         }
-        
         return answer;
     }
-    public void bfs(int[][] com, int i){
-        vis[i] = true;
-        Queue<Integer> q = new LinkedList<>();
-        q.add(i);
-        while(!q.isEmpty()){
-            int idx = q.poll();
-            for(int j = 0; j<com[idx].length;j++){
-                if(idx==j)continue;
-                if(com[idx][j]==0)continue;
-                if(com[idx][j]==1&&vis[j])continue;
-                vis[j] = true;
-                q.add(j);
-            }
+    public void dfs(int[][] computers, int idx, int n, int dep){
+        if(dep==n)return;
+        for(int i = 0; i<n; i++){
+            if(i==idx) continue;
+            if(computers[idx][i]==0|| visit[i])continue;
+            visit[i] = true;
+            dfs(computers, i, n, dep+1);
         }
+        
     }
 }
