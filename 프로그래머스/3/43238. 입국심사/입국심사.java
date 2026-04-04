47228e3ee4359a9bd06854a885d44449ef09e067
kimsy98 <119915194+kimsy98@users.noreply.github.com>
2026-04-04 19:51:50 +0900

    [level 3] Title: 입국심사, Time: 105.49 ms, Memory: 96.3 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,28 +2,22 @@ import java.util.*;
 class Solution {
     public long solution(int n, int[] times) {
         long answer = 0;
-        
         Arrays.sort(times);
-        long left = 0;
+        long left = 1;
         long right = (long)times[times.length-1]*n;
-        
         while(left<=right){
-            long mid = (left+ right)/2;
-            if(getTimeCount(times, mid)>=n){
-                right = mid -1;
-                answer = mid;
-            }else{
+            long mid = (left+right)/2;
+            long count = 0;
+            for(int i = 0; i< times.length; i++){
+                count += mid/times[i];
+            }
+            if(count<n){
                 left = mid+1;
+            }else{
+                answer = mid;
+                right = mid-1;
             }
         }
-        
         return answer;
-    }
-    public long getTimeCount(int[] times, long mid){
-        long timeCnt =0;
-        for(int i = 0 ; i <times.length; i++){
-            timeCnt += mid/times[i];
-        }
-        return timeCnt;
     }
 }
