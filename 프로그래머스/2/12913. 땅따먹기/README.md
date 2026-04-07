e691cb0d4e191c13fc5300b0688c1ad07c3cf483
kimsy98 <119915194+kimsy98@users.noreply.github.com>
2026-04-07 12:23:31 +0900

    [level 2] Title: 땅따먹기, Time: 23.31 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/README.md
+++ 프로그래머스/2/12913. 땅따먹기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 땅따먹기 - 12913 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12913?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12913) 
 
 ### 성능 요약
 
-메모리: 87.9 MB, 시간: 21.32 ms
+메모리: 87.1 MB, 시간: 23.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 15일 18:07:18
+2026년 04월 07일 12:23:28
 
 ### 문제 설명
 
--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,19 +1,22 @@
+import java.util.*;
 class Solution {
     int solution(int[][] land) {
         int answer = 0;
-        
-        for(int i =1; i<land.length;i++){
-            for(int j =0; j<4;j++){
+        int n = land.length-1;
+        for(int i = 1 ;i< land.length; i++){
+            for(int j = 0 ;j< land[0].length; j++){
                 int max = 0;
-                for(int n=0;n<4;n++){
-                    if(n==j)continue;
-                    max = (max>land[i][j]+land[i-1][n]) ? max :land[i][j]+land[i-1][n];
+                for(int k=0; k<land[0].length; k++){
+                    if(j==k) continue;
+                    if(land[i][j]+land[i-1][k]>land[i][j]){
+                        max= Math.max(max, land[i][j]+land[i-1][k]);
+                    }
                 }
                 land[i][j] = max;
-            }
+            }   
         }
-        for(int i =0; i<4; i++){
-            answer = (answer< land[land.length-1][i]) ? land[land.length-1][i] : answer;
+        for(int i = 0; i<land[0].length; i++){
+            answer = Math.max(land[n][i], answer);
         }
         return answer;
     }
