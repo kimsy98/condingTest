4f3fc346ae4b7d9b3fb9e89c4990d386ef2cc69e
kimsy98 <119915194+kimsy98@users.noreply.github.com>
2026-04-09 16:19:48 +0900

    [level 4] Title: 징검다리, Time: 38.89 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -2,39 +2,33 @@ import java.util.*;
 class Solution {
     public int solution(int distance, int[] rocks, int n) {
         int answer = 0;
-
+        int left =1;
+        int right = distance;
         Arrays.sort(rocks);
         
-        int left = 1;
-        int right = distance;
-
         while(left<=right){
-            
             int mid = (right+left)/2;
-            
-            if(checkRockCnt(rocks, distance, mid)<=n){
+            if(getRemoveRockCnt(rocks, distance,mid)<=n){
                 answer = mid;
                 left = mid+1;
             }else{
                 right = mid-1;
             }
-            
         }
-        
-        
         return answer;
     }
-    public int checkRockCnt(int[] rocks, int end, int mid){
-        int removeRockCnt=0;
-        int prevRock=0;
-        for(int i =0; i< rocks.length; i++){
+    public int getRemoveRockCnt(int[] rocks, int end, int mid){
+        int prevRock = 0;
+        int removeRockCnt =0;
+        for(int i =0; i<rocks.length; i++){
             if(rocks[i]-prevRock<mid){
                 removeRockCnt++;
             }else{
                 prevRock = rocks[i];
             }
         }
-        if(end - prevRock<mid) removeRockCnt++;
+        if(end-prevRock<mid) removeRockCnt++;
         return removeRockCnt;
     }
+    
 }
